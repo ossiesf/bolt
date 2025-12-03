d6d3c618b47eb589f0096f10d20a324fde2ae040
Ossie Finnegan <Ossie.finnegan.sf@gmail.com>
2025-12-03 05:41:53 +0000

    All three endpoints now work, and you can create a shortened link that will redirect you to the original Must force HTTP protocol in URL to avoid treating the original URL as a relative path

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
 
 from app.shortener import shorten_url, get_original_url
+from app.models import ShortenRequest, ShortenResponse
+
 app = FastAPI()
 
 @app.get('/{short_code}')
@@ -11,12 +13,14 @@ async def redirect(short_code: str):
     if not original_url:
         raise HTTPException(status_code=404, detail="URL not found - have you shortened it yet?")
     
-    return RedirectResponse(original_url, status_code=302)
+    return RedirectResponse(url=original_url, status_code=302)
 
 @app.get('/original-urls/{code}')
 async def get_url(code: str):
     return get_original_url(code)
 
-@app.post('/shorten')
-async def shorten_request(original_url: str):
-    return shorten_url(original_url)
+@app.post('/shorten', response_model=ShortenResponse)
+async def shorten_request(request: ShortenRequest):
+    short_code = shorten_url(request.url)
+    short_url = 'http://localhost:8000/' + short_code
+    return {'short_code': short_code, 'short_url': short_url}
