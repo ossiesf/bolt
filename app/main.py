feaf93088476f077f61fbbe8b515efeb60fc9434
Ossie Finnegan <Ossie.finnegan.sf@gmail.com>
2025-12-03 04:28:49 +0000

    First draft of logic for the API and the shortener

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 
+from app.shortener import shorten_url, get_original_url
 app = FastAPI()
 
-@app.get('/urls/{code}')
+@app.get('/{short_code}')
+async def redirect(short_code: str):
+    original_url = get_original_url(short_code)
+
+    if not original_url:
+        raise HTTPException(status_code=404, detail="URL not found - have you shortened it yet?")
+    
+    return RedirectResponse(original_url, status_code=302)
+
+@app.get('/original-urls/{code}')
 async def get_url(code: str):
-    return code
+    return get_original_url(code)
 
-@app.get('/api/stats')
-async def stats():
-    return 'stats'
-
-@app.post('/urls')
-async def create_url(url: str):
-    return 'create_url'
+@app.post('/shorten')
+async def shorten_url(original_url: str):
+    return shorten_url(original_url)
